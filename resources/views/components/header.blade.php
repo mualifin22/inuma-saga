442a5536d352daf81387f2ceb8047d74044f5cc5
mualifin22 <mimualifin22@gmail.com>
2024-11-17 16:23:53 +0700

    blade component, model, & migration

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,5 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
-</div>
+<header class="bg-white shadow">
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">{{ $slot }}</h1>
+    </div>
+</header>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,6 @@
 <x-layout>
+
     <x-slot:title>{{ $title }}</x-slot:title>
-    <h3 class="text-xl">This is Blog Page</h3>
+
+    <h3 class="text-xl">This is Home Page</h3>
 </x-layout>
